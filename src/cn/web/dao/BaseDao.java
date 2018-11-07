015c6dea9f6356af913db4eba4c53b011eb60302
Administrator <Administrator@windows10.microdone.cn>
2018-11-07 11:10:18 +0800

    创建BaseDao,完成update delete save的抽取操作（参数应该设置为 Object[]数组）

---
+++ src/cn/web/dao/BaseDao.java
@@ -0,0 +1,30 @@
+package cn.web.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import cn.web.utils.JdbcUtils;
+
+public class BaseDao {
+	//编写一个公共的父类，此类方法用来抽取共性的代码，子dao都需要基层此类
+  protected int update(String sql,Object[] param) {
+	  Connection conn = null;
+	  PreparedStatement pre = null; 
+	  try {
+		conn = JdbcUtils.getConnection(); 
+		pre = conn.prepareStatement(sql);
+		for(int i = 0;i <param.length;i++) {
+			pre.setObject(i+1, param[i]);
+			System.out.println("循环一次");
+		}
+		return pre.executeUpdate();
+	} catch (SQLException e) {
+		throw new RuntimeException(e);
+	}
+	  
+	
+	
+}
+	
+}
